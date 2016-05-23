7a696e77f86e08e7f6f01d1d1b578b1c3683d649
shambhavi3qi <shambhavi@3qilabs.com>
2016-05-23 11:41:32 +0530

    Modified scripts with 1000 lines

--- web/awetest_scripts/google_search_awe35.rb
+++ web/awetest_scripts/google_search_awe35.rb
@@ -6,7 +6,7 @@ module AwetestDslAllDefaults
     set_textfield_by_name(browser, 'q', '3QILabs')
     click_button_by_name(browser, 'btnG')
     sleep(2)
-    for i in 1..50
+    for i in 1..1000
         validate_text(browser, '3Qi Labs')
     end
     browser.close
