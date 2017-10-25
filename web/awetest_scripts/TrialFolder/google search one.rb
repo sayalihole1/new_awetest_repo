43b8dfa9d9ff3670bf31d0410c2431a76db5cc38
sampadha <sampadha@3qilabs.com>
2017-10-25 17:14:15 +0530

    Update google search one.rb

--- web/awetest_scripts/TrialFolder/google search one.rb
+++ web/awetest_scripts/TrialFolder/google search one.rb
@@ -2,10 +2,10 @@ module AwetestDslAllDefaults
   def run
     browser = open_browser
     browser.goto("www.google.com")
-    sleep(2)
+    sleep(1)
     set_textfield_by_name(browser, 'q', '3QILabs')
     click_button_by_name(browser, 'btnG')
-    sleep(2)
+    sleep(1)
     for i in 1..1000
         validate_text(browser, '3Qi Labs')
     end
