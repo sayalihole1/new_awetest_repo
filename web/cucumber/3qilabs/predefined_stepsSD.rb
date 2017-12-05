a547cec7b112b8309453f1b460542740eb624bb9
reema <reema.naik@3qilabs.com>
2017-12-05 17:00:14 +0530

    added extra feature file

--- web/cucumber/3qilabs/predefined_stepsSD.rb
+++ web/cucumber/3qilabs/predefined_stepsSD.rb
@@ -5,7 +5,7 @@ Then /^I click the search button "(.*?)"$/ do |element_text|
 	sleep 1
   @browser.button(:name, element_text).click
 end
-
+###correct file??
 Given /^I connect to the webdriver browser$/ do
 	require 'watir-webdriver'
 	@browser = Watir::Browser.new(:remote, :url=>'http://192.168.1.107:3001/wd/hub/')
