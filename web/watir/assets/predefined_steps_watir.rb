a06ee3eae61c263219463286e8d5ccff9d144de9
shambhavi <shambhavi@3qilabs.com>
2018-01-11 15:07:19 +0530

    Files changed

--- web/watir/assets/predefined_steps_watir.rb
+++ web/watir/assets/predefined_steps_watir.rb
@@ -11,3 +11,6 @@ Given /^I connect to the webdriver browser$/ do
 	@browser = Watir::Browser.new(:remote, :url=>'http://192.168.1.107:3001/wd/hub/')
 	#browser = Watir::Browser.new(:remote, :url=>'http://localhost:8080/wd/hub/')
 end
+
+
+#Sample
