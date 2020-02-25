6a865bcfb005b75b61b4694dee51a82d9e6668b0
suresh3qilabs <suresh.babu@3qilabs.com>
2020-02-25 15:07:06 +0530

    modified one more file

--- web/cucumber/step_definitions/mystepsSD.rb
+++ web/cucumber/step_definitions/mystepsSD.rb
@@ -13,7 +13,7 @@
 #end
 
 #########################################
-
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
 Then(/^I enter text "(.*?)" in the search box$/) do |arg1|
   @browser.text_field(:id,'gbqfq').set("#{arg1}")
 end
