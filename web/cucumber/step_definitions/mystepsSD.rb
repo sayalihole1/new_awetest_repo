e88ffab8b447e37c0ac85ed0754f2f90e8f2d262
suresh3qilabs <suresh.babu@3qilabs.com>
2020-02-25 11:51:09 +0530

    modified same definition file

--- web/cucumber/step_definitions/mystepsSD.rb
+++ web/cucumber/step_definitions/mystepsSD.rb
@@ -24,7 +24,7 @@ Then(/^I click the link with text "(.*?)"$/) do |text|
   @browser.link(:text, "#{text}").click
 end
 
-
+########################################################
 ########################################################
 Then(/^I enter text "(.*?)" in the search box$/) do |arg1|
   @browser.text_field(:id,'gbqfq').set("#{arg1}")
