350a593a8d0b25c5f929a7d599e44da724d6eb19
shambhavi <shambhavi@3qilabs.com>
2020-03-04 14:40:37 +0530

    step def updated

---
+++ web/cucumber/3qilabs/step_definitions/steps_cucuSD.rb
@@ -0,0 +1,12 @@
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
+[remote "origin"]
+	url = git@github.com:3qilabs/awetest.git
