b2679b980b8f78417d0b0b99489999a7999bf415
sampadha <sampadha@3qilabs.com>
2022-01-19 22:41:02 +0530

    Edited files

--- web/watir/assets/mysteps_watir.rb
+++ web/watir/assets/mysteps_watir.rb
@@ -1,16 +1,16 @@
-#Given(/^I open a browser$/) do
-#	require 'rubygems'
-#	require 'watir-webdriver'
-#	@browser = Watir::Browser.new
-#end
+Given(/^I open a browser$/) do
+	require 'rubygems'
+	require 'watir-webdriver'
+	@browser = Watir::Browser.new
+end
 
-#Then(/^I wait for (\d+) seconds$/) do |arg1|
- # sleep arg1.to_i
-#end
+Then(/^I wait for (\d+) seconds$/) do |arg1|
+ sleep arg1.to_i
+end
 
-#Then(/^I go to the url "(.*?)"$/) do |arg1|
-#  @browser.goto("#{arg1}") 
-#end
+Then(/^I go to the url "(.*?)"$/) do |arg1|
+ @browser.goto("#{arg1}")
+end
 
 Then(/^I enter text "(.*?)" in the search box$/) do |arg1|
   @browser.text_field(:id,'gbqfq').set("#{arg1}")
