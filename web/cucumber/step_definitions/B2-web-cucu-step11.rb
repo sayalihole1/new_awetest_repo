7a740f4a237cb6d2b1d1642fdbde95ea93ca8602
sampadha <sampadha@3qilabs.com>
2022-03-22 15:50:00 +0530

    Removed commented code

--- web/cucumber/step_definitions/B2-web-cucu-step11.rb
+++ web/cucumber/step_definitions/B2-web-cucu-step11.rb
@@ -1,17 +1,3 @@
-# Given(/^I open a browser$/) do
-# 	require 'rubygems'
-# 	require 'watir-webdriver'
-# 	@browser = Watir::Browser.new
-# end
-# 
-# Then(/^I wait for (\d+) seconds$/) do |arg1|
-#  sleep arg1.to_i
-# end
-#
-# Then(/^I go to the url "(.*?)"$/) do |arg1|
-#  @browser.goto("#{arg1}")
-# end
-
 Then(/^I enter text "(.*?)" in the search box$/) do |arg1|
   @browser.text_field(:id,'gbqfq').set("#{arg1}")
 end
